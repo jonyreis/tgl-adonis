06d645acedfdf8cda8cd180bbb75cc0ac67233f4
Jony <jonyreiscardoso@gmail.com>
2021-06-07 15:24:36 -0400

    feat: add Game Model / Schema

---
+++ database/migrations/1623089397427_game_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class GameSchema extends Schema {
+  up () {
+    this.create('games', (table) => {
+      table.increments()
+      table.string('type', 80).notNullable().unique()
+      table.text('description').notNullable()
+      table.integer('range')
+      table.integer('max_number')
+      table.integer('max_cart_value')
+      table.string('color')
+      table.float('price')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('games')
+  }
+}
+
+module.exports = GameSchema
