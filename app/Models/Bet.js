14de7c2a15c3e34ff985737da1a7fd085fe2c925
Jony <jonyreiscardoso@gmail.com>
2021-06-08 08:49:41 -0400

    feat: add relationship between models

--- app/Models/Bet.js
+++ app/Models/Bet.js
@@ -4,6 +4,13 @@
 const Model = use('Model')
 
 class Bet extends Model {
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  game () {
+    return this.belongsTo('App/Models/Game')
+  }
 }
 
 module.exports = Bet
--- app/Models/Game.js
+++ app/Models/Game.js
@@ -4,6 +4,9 @@
 const Model = use('Model')
 
 class Game extends Model {
+  bet () {
+    return this.belongsTo('App/Models/Bet')
+  }
 }
 
 module.exports = Game
