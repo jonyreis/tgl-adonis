07f05ee8921a2a8ffc6f570d4bc6e91fdd309677
Jony <jonyreiscardoso@gmail.com>
2021-06-09 13:21:47 -0400

    freat: add Mail send on UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,41 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class UserController {
-  async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+  async index () {
+    const users = await User.all()
 
-    const user = await User.create(data)
+    return users
+  }
 
-    return user
+  async store ({ request, response }) {
+    try {
+      const data = request.only(['username', 'email', 'password'])
+      const user = await User.create(data)
+  
+      await user.save()
+  
+      await Mail.send(
+        ['emails.user'],
+        {
+          email: data.email,
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('jonyreiscardoso@gmail.com', 'Jony Reis')
+            .subject('Novo Usuário')
+        }
+      )
+
+      return user
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo não deu certo, ao fazer o cadastro'} })
+    }
   }
 }
 
